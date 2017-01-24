91452c9b937141113405d89ac4d917aa2cec9476
Warren Wilkinson <wwilkinson@dwavesys.com>
2017-01-24 12:14:25 -0800

    Relicense under CC0 (public domain)

--- test.lisp
+++ test.lisp
@@ -1,26 +1,3 @@
-
-;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: CHANGED-STREAM.TEST; Base: 10 -*-
-
-;;; Copyright (c) 2012, Warren Wilkinson.  All rights reserved.
-
-;;; BEGIN_LICENSE:LGPL2
-;;;
-;;; This library is free software: you can redistribute it and/or modify
-;;; it under the terms of the GNU Library General Public License as published by
-;;; the Free Software Foundation; version 2.
-;;;
-;;; This library is distributed in the hope that it will be useful,
-;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;; GNU Library General Public License for more details.
-;;;
-;;; You should have received a copy of the GNU Library General Public License
-;;; along with this library; see the file COPYING.LIB.  If not, write to
-;;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-;;; Boston, MA 02110-1301, USA.
-;;;
-;;; END_LICENSE
-
 (defpackage :read-csv.test
   (:use :common-lisp :read-csv)
   (:export run-tests))
